6461203edd8b3b7cada064de82f8117ab75edb9f
Doug Dresser <Doug@Dougs-Macbook-Pro-2.local>
2018-03-20 22:01:52 -0500

    Removed old test dirs

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-from django.conf import settings
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.project.settings')
-
-def pytest_configure():
-    settings.DEBUG = False
-    settings.configure()
