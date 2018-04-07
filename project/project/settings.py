96e3a166d0a8b0c50dd6038adbdbeeba7d890a3e
Doug Dresser <Doug@Dougs-Macbook-Pro-2.local>
2018-04-07 17:32:09 -0500

    Got static files to work

--- project/project/settings.py
+++ project/project/settings.py
@@ -150,6 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, 'templates/static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static/')]
 #STATIC_URL = '/static/'
 
 # Twilio settings
